2f897480503be9a74da61de3c83703571228d173
fatma <fatemamahmoud2004@gmail.com>
2025-03-09 04:57:22 +0200

    Implement Huffman code generation logic

--- Data_Compression/src/main/java/com/example/data_compression/logic/Hoffman.java
+++ Data_Compression/src/main/java/com/example/data_compression/logic/Hoffman.java
@@ -1,7 +1,7 @@
 package com.example.data_compression.logic;
-
+import java.util.HashMap;
 public class Hoffman {
-    private class HoffmanNode {
+    private  class HoffmanNode {
         public char character;
         public int frequency;
         public HoffmanNode left, right;
@@ -9,5 +9,26 @@ public class Hoffman {
             this.character = character;
             this.frequency = frequency;
         }
+    }
+ private static HashMap<Character, String> generateCodes(HoffmanNode root){
+        if (root==null)
+            return null;
+        HashMap<Character, String> Codes = new HashMap<>();
+        StringBuilder binaryCode = new StringBuilder();
+        recursiveHelper(root,binaryCode,Codes);
+        return Codes;
+    }
+    private static void recursiveHelper(HoffmanNode node,StringBuilder binaryCode,HashMap<Character, String> Codes){
+        if(node.left==null && node.right==null){
+            Codes.put(node.character,binaryCode.toString());
+            return;
+        }
+        binaryCode.append("1");
+        recursiveHelper(node.right,binaryCode,Codes);
+        int length = binaryCode.length();
+        binaryCode.deleteCharAt(length-1);
+        binaryCode.append("0");
+        recursiveHelper(node.left,binaryCode,Codes);
+        binaryCode.deleteCharAt(length-1);
     }
 }
