749d2cda69ce3acff9bdb54c2c4a46c7eab068a8
Mohamed8Zanaty <mohamed8zanaty@example.com>
2025-03-11 02:30:21 +0200

    solving conflicts

--- Data_Compression/src/main/java/com/example/data_compression/logic/Huffman.java
+++ Data_Compression/src/main/java/com/example/data_compression/logic/Huffman.java
@@ -43,4 +43,35 @@ public class Huffman {
         recursiveHelper(node.left,binaryCode,Codes);
         binaryCode.deleteCharAt(length-1);
     }
+
+    //give each pixel binary code (less freq more bits)-(more freq less bits)
+    public void generateHuffmanCodes(HuffmanNode curNode, String binCode, Map<Integer, String> pixelsCode) {
+        if (curNode.right == null || curNode.left == null) {
+            pixelsCode.put(curNode.frequency, binCode);
+            return;
+        }
+        if (curNode.right != null) {
+            generateHuffmanCodes(curNode.right, binCode += "1", pixelsCode);
+        }
+        if (curNode.left != null) {
+            generateHuffmanCodes(curNode.left, binCode += "0", pixelsCode);
+        }
+
+    }
+
+    // save huffman tree to be used in decoding as string
+    // stringbuider for better memmory
+    public void serializeHuffmanTree(HuffmanNode curNode, StringBuilder savedTree) {
+        if (curNode.right == null && curNode.left == null) {
+            savedTree.append("P").append(curNode.character).append(" ");
+            return;
+        } else {
+            savedTree.append("OSC"); //internal node
+
+        }
+        serializeHuffmanTree( curNode.right, savedTree);
+        serializeHuffmanTree( curNode.left, savedTree);
+
+    }
+
 }
