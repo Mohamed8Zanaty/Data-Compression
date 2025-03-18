54e316780fefd83c774cacd14b7c90da2c09df9b
Mohamed8Zanaty <mohamed.zanaty8805@gmail.com>
2025-03-18 23:53:49 +0200

    temp3

--- Data_Compression/src/main/java/com/example/data_compression/logic/FileHandler.java
+++ Data_Compression/src/main/java/com/example/data_compression/logic/FileHandler.java
@@ -1,5 +1,7 @@
 package com.example.data_compression.logic;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -26,22 +28,23 @@ public class FileHandler {
 
     }
   // Write encoded Image and Huffman codes to a file
-    public static void writeCompressedImage(String encodedImage, Map<Integer, String> huffmanCodes, String filePath) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            // Write Huffman codes (each pixel and its corresponding code)
-            writer.write("Huffman Codes:\n");
-            for (Map.Entry<Integer, String> entry : huffmanCodes.entrySet()) {
-                writer.write(entry.getKey() + ":" + entry.getValue() + "\n");
-            }
+//    public static void writeCompressedImage(String encodedImage, Map<Integer, String> huffmanCodes, String filePath) {
+//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+//            // Write Huffman codes (each pixel and its corresponding code)
+//            writer.write("Huffman Codes:\n");
+//            for (Map.Entry<Integer, String> entry : huffmanCodes.entrySet()) {
+//                writer.write(entry.getKey() + ":" + entry.getValue() + "\n");
+//            }
+//
+//            writer.write("Encoded Image:\n");
+//            writer.write(encodedImage);
+//
+//            System.out.println("Data successfully written to " + filePath);
+//        } catch (IOException e) {
+//            System.out.println("Error writing to file: " + e.getMessage());
+//        }
+//    }
 
-            writer.write("Encoded Image:\n");
-            writer.write(encodedImage);
-
-            System.out.println("Data successfully written to " + filePath);
-        } catch (IOException e) {
-            System.out.println("Error writing to file: " + e.getMessage());
-        }
-    }
     public static int[][] readImage(String path) throws IOException {
         BufferedImage image = ImageIO.read(new File(path));
         int width = image.getWidth();
