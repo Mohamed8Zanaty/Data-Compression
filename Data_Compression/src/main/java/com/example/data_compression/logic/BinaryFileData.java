86104f5e5b81ab69498178f0e120510aa4ef227b
blabla <nvfkdb@gmail.com>
2025-11-16 23:34:54 +0200

    edit: refactor Huffman

--- Data_Compression/src/main/java/com/example/data_compression/logic/BinaryFileData.java
+++ Data_Compression/src/main/java/com/example/data_compression/logic/BinaryFileData.java
@@ -1,25 +1,3 @@
 package com.example.data_compression.logic;
 
-public class BinaryFileData {
-    private String data;
-    private int size;
-    private Huffman.HuffmanNode rootNode;
-
-    public BinaryFileData(String data, int size, Huffman.HuffmanNode rootNode) {
-        this.data = data;
-        this.size = size;
-        this.rootNode = rootNode;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public Huffman.HuffmanNode getRootNode() {
-        return rootNode;
-    }
-}
+public record BinaryFileData(String data, int size, Huffman.HuffmanNode rootNode) { }
--- Data_Compression/src/main/java/com/example/data_compression/logic/HuffmanDemo.java
+++ Data_Compression/src/main/java/com/example/data_compression/logic/HuffmanDemo.java
@@ -199,12 +199,5 @@ public class HuffmanDemo {
 
     }
 
-    public static void main(String[] args) {
-        try {
-
-        }catch (Exception e) {
-            System.out.println("Error");
-        }
-    }
 
 }
